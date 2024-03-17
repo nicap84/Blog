5f0a80717cbf916d8014a4e615c52b021b7be345
Maria Marin <mtmarin84@gmail.com>
2024-03-17 18:59:02 +0000

    initial commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = new express();
+const path = require('path');
+
+app.listen(4000, () => {
+    console.log('App listening on port 4000');
+});
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'pages/index.html'));
+});
+
+app.get('/about', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'pages/about.html'));
+});
+
+app.get('/contact', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'pages/contact.html'));
+})
+
+app.get('/post', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'pages/post.html'));
+})
