70f9c3f1b187d55fb776e3089e085dc8908f8d85
Maria Marin <mtmarin84@gmail.com>
2024-07-21 22:48:55 +0200

    * Add body-parser and * Creates new blogPosts

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const blogPost = require('./models/blogPost');
 
 const app = new express();
 
-mongoose.connect('mongodb://localhost/my_database', {useNewUrlParser: true});
+mongoose.connect('mongodb://127.0.0.1:27017/my_database', { useNewUrlParser: true })
+    .then(() => console.log('Connected'))
+    .catch(err => console.log(err));
 
 app.set('view engine', 'ejs');  
 
@@ -12,6 +16,8 @@ app.listen(4000, () => {
 });
 
 app.use(express.static('public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
 
 app.get('/', (req, res) => {
     res.render('index');
@@ -31,4 +37,13 @@ app.get('/post', (req, res) => {
 
 app.get('/post/new', (req, res) => {
     res.render('create');
+})
+
+app.post('/post/new', (req, res) => {
+    const { title, body } = req.body;
+    if (!title || !body) {
+        return res.status(400).send('Title and body are required.');
+    }
+    blogPost.create(req.body);
+    res.redirect('/'); 
 })
