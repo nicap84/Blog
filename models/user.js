e75097f488cd44a22e3855212d3093bc863ff17d
Maria Marin <mtmarin84@gmail.com>
2024-08-07 17:24:47 +0200

    * encrypt the password

--- models/user.js
+++ models/user.js
@@ -1,8 +1,17 @@
 import mongoose from "mongoose";
+import bcrypt from "bcrypt";
 
 const userSchema = new mongoose.Schema({
     userName: String,
     password: String,
 });
+
+userSchema.pre('save', function(next){
+    const user = this
+    bcrypt.hash(user.password, 10, (error, hash) => {
+    user.password = hash
+    next()
+    })
+})
 
 export const userModel = mongoose.model('User', userSchema);
