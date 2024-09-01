d6d50454402c381203572aa72642c11309a08775
Maria Marin <mtmarin84@gmail.com>
2024-09-01 20:38:43 +0200

    * improve the error message

--- models/user.js
+++ models/user.js
@@ -6,12 +6,12 @@ import uniqueValidator from "mongoose-unique-validator";
 const userSchema = new mongoose.Schema({
     userName: {
         type: String,
-        required: true,
+        required: [true, 'Please provide username'],
         unique: true
     },
     password:{
         type: String,
-        required: true
+        required: [true, 'Please provide password'],
     }
 });
 
